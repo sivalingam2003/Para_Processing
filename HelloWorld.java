5c01302e3e69e360ff7bb8a638acc0d86b3cb093
sivalingam2003 <sivalingamc.21mech@kongu.edu>
2025-02-26 17:27:54 +0530

    commit

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+class HelloWorld{
+	public static void main(String[] Args){
+		// System.out.println("Hello World7667sq");
+		int n=5;
+		int fact=1;
+		int i=1;
+		while(n<=5){
+			fact=fact*i;
+			i++;
+		}
+		System.out.println(fact);
+	}
+}
